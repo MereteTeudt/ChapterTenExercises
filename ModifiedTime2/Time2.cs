b946db07da60ee00850968b6dabbcfced8f0c759
MereteTeudt <mere0312@edu.campusvejle.dk>
2018-08-17 12:03:14 +0200

    Added word file

--- ModifiedTime2/Time2.cs
+++ ModifiedTime2/Time2.cs
@@ -8,5 +8,27 @@ namespace ModifiedTime2
 {
     class Time2
     {
+        private int secondsFromMidnight;
+        public Time2(int secondsFromMidnight)
+        {
+            SetTime(secondsFromMidnight);
+        }
+        public Time2(Time2 time)
+            : this(time.secondsFromMidnight) { }
+        public void SetTime(int hour, int minute, int second)
+        {
+            SecondsFromMidnight = hour * 3600 + minute * 60 + second;
+        }
+        public int SecondsFromMidnight
+        {
+            get
+            {
+                return
+            }
+            set
+            {
+
+            }
+        }
     }
 }
