1f20ef112a00009da298a6971e753517573ed1dd
MereteTeudt <mere0312@edu.campusvejle.dk>
2018-08-20 11:24:54 +0200

    DepreciatingValue

---
+++ DepreciatingValue/DepreciatingValue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DepreciatingValue
+{
+    class DepreciatingValue
+    {
+        public int ValueOfAsset { get; set; }
+        public static int AnnualDepreciationValue { get; set; }
+
+        public DepreciatingValue(int assetValue)
+        {
+            ValueOfAsset = assetValue;
+        }
+        public void CalculateAnnualDepreciation()
+        {
+            int percent = ValueOfAsset / 100 * AnnualDepreciationValue;
+            ValueOfAsset -= percent;
+        }
+    }
+}
---
+++ DepreciatingValue/DepreciatingValueTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DepreciatingValue
+{
+    class DepreciatingValueTest
+    {
+        static void Main(string[] args)
+        {
+            DepreciatingValue asset1 = new DepreciatingValue(5000000);
+            DepreciatingValue asset2 = new DepreciatingValue(6000000);
+            DepreciatingValue asset3 = new DepreciatingValue(7000000);
+
+            DepreciatingValue.AnnualDepreciationValue = 10;
+
+            asset1.CalculateAnnualDepreciation();
+            Console.WriteLine($"{asset1.ValueOfAsset:C}");
+            asset2.CalculateAnnualDepreciation();
+            Console.WriteLine($"{asset2.ValueOfAsset:C}");
+            asset3.CalculateAnnualDepreciation();
+            Console.WriteLine($"{asset3.ValueOfAsset:C}");
+
+            DepreciatingValue.AnnualDepreciationValue = 11;
+
+            asset1.CalculateAnnualDepreciation();
+            asset1.CalculateAnnualDepreciation();
+            asset1.CalculateAnnualDepreciation();
+            asset2.CalculateAnnualDepreciation();
+            asset2.CalculateAnnualDepreciation();
+            asset2.CalculateAnnualDepreciation();
+            asset3.CalculateAnnualDepreciation();
+            asset3.CalculateAnnualDepreciation();
+            asset3.CalculateAnnualDepreciation();
+            Console.WriteLine($"{asset1.ValueOfAsset:C}");
+            Console.WriteLine($"{asset2.ValueOfAsset:C}");
+            Console.WriteLine($"{asset3.ValueOfAsset:C}");
+
+            Console.ReadLine();
+        }
+    }
+}
