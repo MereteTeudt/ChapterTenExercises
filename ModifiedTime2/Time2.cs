e38cb644f847b50c6d4a8a4c09014ba0550e126f
MereteTeudt <mere0312@edu.campusvejle.dk>
2018-08-20 10:24:46 +0200

    10.4 Done (excluding the option to give an object as a parameter)

--- ModifiedTime2/Time2.cs
+++ ModifiedTime2/Time2.cs
@@ -13,11 +13,10 @@ namespace ModifiedTime2
         {
             SetTime(hour, minute, second);
         }
-        public Time2(Time2 time)
-            : this(time.secondsFromMidnight) { }
+
         public void SetTime(int hour, int minute, int second)
         {
-            SecondsFromMidnight = hour * 3600 + minute * 60 + second;
+            SecondsFromMidnight = (hour * 3600) + (minute * 60) + second;
         }
         public int SecondsFromMidnight
         {
@@ -27,8 +26,35 @@ namespace ModifiedTime2
             }
             set
             {
+                if (value < 0 || value > 86400)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value),
+                        value, $"{nameof(SecondsFromMidnight)} must be 0-86400");
+                }
                 secondsFromMidnight = value;
             }
+        }
+        public string ToUniversalString()
+        {
+            TimeSpan t = TimeSpan.FromSeconds(SecondsFromMidnight);
+            return t.ToString(@"hh\:mm\:ss");
+        }
+
+        public override string ToString()
+        {
+            string time;
+            if(secondsFromMidnight > 43200)
+            {
+                int pmTime = SecondsFromMidnight - 43200;
+                TimeSpan t = TimeSpan.FromSeconds(pmTime);
+                time = t.ToString(@"hh\:mm\:ss") + "pm";
+            }
+            else
+            {
+                TimeSpan t = TimeSpan.FromSeconds(SecondsFromMidnight);
+                time = t.ToString(@"hh\:mm\:ss") + "am";
+            }
+            return time;
         }
     }
 }
--- ModifiedTime2/Time2Test.cs
+++ ModifiedTime2/Time2Test.cs
@@ -10,6 +10,41 @@ namespace ModifiedTime2
     {
         static void Main(string[] args)
         {
+            var t1 = new Time2();
+            var t2 = new Time2(2);
+            var t3 = new Time2(21, 34);
+            var t4 = new Time2(12, 25, 42);
+
+            Console.WriteLine("Constructed with: \n");
+            Console.WriteLine("t1: all arguments defaulted");
+            Console.WriteLine($" {t1.ToUniversalString()}");
+            Console.WriteLine($" {t1.ToString()}\n");
+
+            Console.WriteLine("t2: hour specified; minute and second defaulted");
+            Console.WriteLine($" {t2.ToUniversalString()}");
+            Console.WriteLine($" {t2.ToString()}");
+
+            Console.WriteLine("t3: hour and minute specified; second defaulted");
+            Console.WriteLine($" {t3.ToUniversalString()}");
+            Console.WriteLine($" {t3.ToString()}");
+
+            Console.WriteLine("t4: hour, minute and second specified");
+            Console.WriteLine($" {t4.ToUniversalString()}");
+            Console.WriteLine($" {t4.ToString()}");
+
+
+
+            try
+            {
+                var t6 = new Time2(27, 74, 99);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine("\nException while initializing t6");
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
         }
     }
 }
